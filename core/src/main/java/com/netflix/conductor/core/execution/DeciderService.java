42a4d5a67f8cbb0f782afcc978e42c8873e77889
Pascal-Joos <pascal.joos@outlook.de>
2025-12-02 13:49:37 +0000

    Revert "fix: 1 - DEREFERENCE_NULLABLE - return taskMappers - dereferenced expression taskMappers                 .getOrDefault(type, taskMappers.get(USER_DEFINED.name())) is @Nullable"
    This reverts commit 42c235c35118294fb4f1fef9dea9f47454d83c67.

--- core/src/main/java/com/netflix/conductor/core/execution/DeciderService.java
+++ core/src/main/java/com/netflix/conductor/core/execution/DeciderService.java
@@ -865,14 +865,10 @@ public class DeciderService {
         // fork.
         // A new task must only be scheduled if a task, with the same reference name is not already
         // in this workflow instance
-        TaskMapper taskMapper = taskMappers.get(type);
-        if (taskMapper == null) {
-            taskMapper =
-                    java.util.Objects.requireNonNull(
-                            taskMappers.get(USER_DEFINED.name()),
-                            "USER_DEFINED task mapper must be configured");
-        }
-        return taskMapper.getMappedTasks(taskMapperContext).stream()
+        return taskMappers
+                .getOrDefault(type, taskMappers.get(USER_DEFINED.name()))
+                .getMappedTasks(taskMapperContext)
+                .stream()
                 .filter(task -> !tasksInWorkflow.contains(task.getReferenceTaskName()))
                 .collect(Collectors.toList());
     }
