fb44c93aad9dc9665926081fd354196b8135e54d
Pascal-Joos <pascal.joos@outlook.de>
2025-12-02 14:17:30 +0000

    fix: 14 - UNBOX_NULLABLE - int version = (int) input.get("subWorkflowVersion"); - unboxing of a @Nullable value - input.get("subWorkflowVersion")

--- core/src/main/java/com/netflix/conductor/core/execution/tasks/SubWorkflow.java
+++ core/src/main/java/com/netflix/conductor/core/execution/tasks/SubWorkflow.java
@@ -52,7 +52,17 @@ public class SubWorkflow extends WorkflowSystemTask {
     public void start(WorkflowModel workflow, TaskModel task, WorkflowExecutor workflowExecutor) {
         Map<String, Object> input = task.getInputData();
         String name = input.get("subWorkflowName").toString();
-        int version = (int) input.get("subWorkflowVersion");
+        int version = 1;
+        Object versionObj = input.get("subWorkflowVersion");
+        if (versionObj instanceof Number) {
+            version = ((Number) versionObj).intValue();
+        } else if (versionObj != null) {
+            try {
+                version = Integer.parseInt(versionObj.toString());
+            } catch (NumberFormatException e) {
+                LOGGER.warn("Invalid subWorkflowVersion value: {}", versionObj, e);
+            }
+        }
 
         WorkflowDef workflowDefinition = null;
         if (input.get("subWorkflowDefinition") != null) {
