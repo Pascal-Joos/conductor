8c81f7d383e387fccb67d2d733bb42e613f86a70
Pascal-Joos <pascal.joos@outlook.de>
2025-12-02 15:03:14 +0000

    Revert "fix: 24 - DEREFERENCE_NULLABLE - Monitors.recordAckTaskError(task.getTaskType()); - dereferenced expression task is @Nullable"
    This reverts commit 0cca310b0ed275ea49d90b43b9f2c5ca3bab6de4.

--- core/src/main/java/com/netflix/conductor/service/TaskServiceImpl.java
+++ core/src/main/java/com/netflix/conductor/service/TaskServiceImpl.java
@@ -172,12 +172,8 @@ public class TaskServiceImpl implements TaskService {
             String errorMsg = String.format("Error when trying to ack task %s", taskId);
             LOGGER.error(errorMsg, e);
             Task task = executionService.getTask(taskId);
-            if (task != null) {
-                Monitors.recordAckTaskError(task.getTaskType());
-                failTask(task, errorMsg);
-            } else {
-                Monitors.recordAckTaskError("UNKNOWN");
-            }
+            Monitors.recordAckTaskError(task.getTaskType());
+            failTask(task, errorMsg);
             ackResult.set(false);
         }
         return ackResult.get();
