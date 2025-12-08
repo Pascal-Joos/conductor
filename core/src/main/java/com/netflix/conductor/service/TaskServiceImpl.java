85f926be19e5e2ace9a7f4f1e1701b9c9488ff78
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 12:40:23 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression task is @Nullable' /home/vscode/nullness-benchmarks/conductor/core/src/main/java/com/netflix/conductor/service/TaskServiceImpl.java:175             Monitors.recordAckTaskError(task.getTaskType());

--- core/src/main/java/com/netflix/conductor/service/TaskServiceImpl.java
+++ core/src/main/java/com/netflix/conductor/service/TaskServiceImpl.java
@@ -172,8 +172,12 @@ public class TaskServiceImpl implements TaskService {
             String errorMsg = String.format("Error when trying to ack task %s", taskId);
             LOGGER.error(errorMsg, e);
             Task task = executionService.getTask(taskId);
-            Monitors.recordAckTaskError(task.getTaskType());
-            failTask(task, errorMsg);
+            if (task != null) {
+                Monitors.recordAckTaskError(task.getTaskType());
+                failTask(task, errorMsg);
+            } else {
+                LOGGER.error("Unable to retrieve task {} for ack failure handling", taskId);
+            }
             ackResult.set(false);
         }
         return ackResult.get();
