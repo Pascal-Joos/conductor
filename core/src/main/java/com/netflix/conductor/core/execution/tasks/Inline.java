eb8c07260a0f3e48600a16a97a42b9c8ef3b0b65
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 14:46:48 +0000

    Revert "fix: 10 - DEREFERENCE_NULLABLE - Object evalResult = evaluator.evaluate(expression, taskInput); - dereferenced expression evaluator is @Nullable"
    This reverts commit 3c86cfa8c30164ccea2bae122599ba7112a30c58.

--- core/src/main/java/com/netflix/conductor/core/execution/tasks/Inline.java
+++ core/src/main/java/com/netflix/conductor/core/execution/tasks/Inline.java
@@ -79,9 +79,6 @@ public class Inline extends WorkflowSystemTask {
             checkEvaluatorType(evaluatorType);
             checkExpression(expression);
             Evaluator evaluator = evaluators.get(evaluatorType);
-            if (evaluator == null) {
-                throw new IllegalArgumentException("No evaluator found for type: " + evaluatorType);
-            }
             Object evalResult = evaluator.evaluate(expression, taskInput);
             task.addOutput("result", evalResult);
             task.setStatus(TaskModel.Status.COMPLETED);
