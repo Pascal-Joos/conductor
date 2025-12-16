ce6085de01c9a416d25d736c429b1a82d0e91c80
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 15:03:49 +0000

    fix: 24 - DEREFERENCE_NULLABLE - Monitors.recordAckTaskError(task.getTaskType()); - dereferenced expression task is @Nullable

--- core/src/main/java/com/netflix/conductor/service/TaskServiceImpl.java
+++ core/src/main/java/com/netflix/conductor/service/TaskServiceImpl.java
@@ -172,8 +172,10 @@ public class TaskServiceImpl implements TaskService {
             String errorMsg = String.format("Error when trying to ack task %s", taskId);
             LOGGER.error(errorMsg, e);
             Task task = executionService.getTask(taskId);
-            Monitors.recordAckTaskError(task.getTaskType());
-            failTask(task, errorMsg);
+            if (task != null) {
+                Monitors.recordAckTaskError(task.getTaskType());
+                failTask(task, errorMsg);
+            }
             ackResult.set(false);
         }
         return ackResult.get();
