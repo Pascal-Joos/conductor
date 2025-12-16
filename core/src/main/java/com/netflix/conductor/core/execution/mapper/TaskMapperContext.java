f88493d822b59ff06c1bec7e813faae3f594505d
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 14:39:40 +0000

    Revert "fix: 5 - METHOD_NO_INIT - private Builder() {} - initializer method does not guarantee @NonNull fields workflowModel (line 193), workflowTask (line 195), taskInput (line 196), deciderService (line 200) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 2873f361109e18b7d2ef7a381f3edb53a6919615.

--- core/src/main/java/com/netflix/conductor/core/execution/mapper/TaskMapperContext.java
+++ core/src/main/java/com/netflix/conductor/core/execution/mapper/TaskMapperContext.java
@@ -199,12 +199,7 @@ public class TaskMapperContext {
         @Nullable private String taskId;
         private DeciderService deciderService;
 
-        private Builder() {
-            this.workflowModel = new WorkflowModel();
-            this.workflowTask = new WorkflowTask();
-            this.taskInput = new HashMap<>();
-            this.deciderService = new DeciderService();
-        }
+        private Builder() {}
 
         /**
          * Sets the {@code workflowModel} and returns a reference to this Builder so that the
