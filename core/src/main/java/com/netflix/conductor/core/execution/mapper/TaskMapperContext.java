4545353132041a5e680f32a85fc455320cdbe70c
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 12:10:16 +0000

    fix: Type='ASSIGN_FIELD_NULLABLE', message='assigning @Nullable expression to @NonNull field' /home/vscode/nullness-benchmarks/conductor/core/src/main/java/com/netflix/conductor/core/execution/mapper/TaskMapperContext.java:45         taskId = builder.taskId;

--- core/src/main/java/com/netflix/conductor/core/execution/mapper/TaskMapperContext.java
+++ core/src/main/java/com/netflix/conductor/core/execution/mapper/TaskMapperContext.java
@@ -42,7 +42,7 @@ public class TaskMapperContext {
         taskInput = builder.taskInput;
         retryCount = builder.retryCount;
         retryTaskId = builder.retryTaskId;
-        taskId = builder.taskId;
+        taskId = builder.taskId == null ? "" : builder.taskId;
         deciderService = builder.deciderService;
     }
 
