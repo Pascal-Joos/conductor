0806e3e601bb14b8e219a708d7e2f3af95a1156c
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 12:18:51 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression paramString is @Nullable' /home/vscode/nullness-benchmarks/conductor/core/src/main/java/com/netflix/conductor/core/utils/ParametersUtils.java:228         String[] values = paramString.split("(?=(?<!\\$)\\$\\{)|(?<=})");

--- core/src/main/java/com/netflix/conductor/core/utils/ParametersUtils.java
+++ core/src/main/java/com/netflix/conductor/core/utils/ParametersUtils.java
@@ -225,6 +225,9 @@ public class ParametersUtils {
             @Nullable String paramString,
             DocumentContext documentContext,
             @Nullable String taskId) {
+        if (paramString == null) {
+            return "";
+        }
         String[] values = paramString.split("(?=(?<!\\$)\\$\\{)|(?<=})");
         Object[] convertedValues = new Object[values.length];
         for (int i = 0; i < values.length; i++) {
