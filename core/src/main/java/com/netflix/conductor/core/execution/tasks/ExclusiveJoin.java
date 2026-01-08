3222fb80685ecfc37d829dde8686ab92da217a58
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 09:37:22 +0000

    fix: 9 - DEREFERENCE_NULLABLE - for (String joinOnRef : joinOn) { - enhanced-for expression joinOn is @Nullable

--- core/src/main/java/com/netflix/conductor/core/execution/tasks/ExclusiveJoin.java
+++ core/src/main/java/com/netflix/conductor/core/execution/tasks/ExclusiveJoin.java
@@ -24,6 +24,8 @@ import com.netflix.conductor.core.execution.WorkflowExecutor;
 import com.netflix.conductor.model.TaskModel;
 import com.netflix.conductor.model.WorkflowModel;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
+
 import static com.netflix.conductor.common.metadata.tasks.TaskType.TASK_TYPE_EXCLUSIVE_JOIN;
 
 @Component(TASK_TYPE_EXCLUSIVE_JOIN)
@@ -51,12 +53,12 @@ public class ExclusiveJoin extends WorkflowSystemTask {
             // If exclusive join is part of loop over task, wait for specific iteration to get
             // complete
             joinOn =
-                    joinOn.stream()
+                    Nullability.castToNonnull(joinOn).stream()
                             .map(name -> TaskUtils.appendIteration(name, task.getIteration()))
                             .collect(Collectors.toList());
         }
         TaskModel exclusiveTask = null;
-        for (String joinOnRef : joinOn) {
+        for (String joinOnRef : Nullability.castToNonnull(joinOn)) {
             LOGGER.debug("Exclusive Join On Task {} ", joinOnRef);
             exclusiveTask = workflow.getTaskByRefName(joinOnRef);
             if (exclusiveTask == null || exclusiveTask.getStatus() == TaskModel.Status.SKIPPED) {
