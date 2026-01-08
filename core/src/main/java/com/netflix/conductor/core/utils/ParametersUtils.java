0a30a9aab88cb7c634ed1c7a36f703619a0b9103
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 09:44:27 +0000

    Revert "fix: 15 - DEREFERENCE_NULLABLE - String[] values = paramString.split("(?=(?<!\\$)\\$\\{)|(?<=})"); - dereferenced expression paramString is @Nullable"
    This reverts commit 301d0b4b162a909de8bec794b710dea971a37074.

--- core/src/main/java/com/netflix/conductor/core/utils/ParametersUtils.java
+++ core/src/main/java/com/netflix/conductor/core/utils/ParametersUtils.java
@@ -40,7 +40,6 @@ import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.Option;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 
 /** Used to parse and resolve the JSONPath bindings in the workflow and task definitions. */
 @Component
@@ -226,7 +225,7 @@ public class ParametersUtils {
             @Nullable String paramString,
             DocumentContext documentContext,
             @Nullable String taskId) {
-        String[] values = Nullability.castToNonnull(paramString).split("(?=(?<!\\$)\\$\\{)|(?<=})");
+        String[] values = paramString.split("(?=(?<!\\$)\\$\\{)|(?<=})");
         Object[] convertedValues = new Object[values.length];
         for (int i = 0; i < values.length; i++) {
             convertedValues[i] = values[i];
