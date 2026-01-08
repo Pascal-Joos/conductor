c2b4821bad2e065238f2915ac0cc8d65a2bdfd35
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 09:40:48 +0000

    fix: 12 - DEREFERENCE_NULLABLE - for (String joinOnRef : joinOn) { - enhanced-for expression joinOn is @Nullable

--- core/src/main/java/com/netflix/conductor/core/execution/tasks/Join.java
+++ core/src/main/java/com/netflix/conductor/core/execution/tasks/Join.java
@@ -22,6 +22,8 @@ import com.netflix.conductor.core.execution.WorkflowExecutor;
 import com.netflix.conductor.model.TaskModel;
 import com.netflix.conductor.model.WorkflowModel;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
+
 import static com.netflix.conductor.common.metadata.tasks.TaskType.TASK_TYPE_JOIN;
 
 @Component(TASK_TYPE_JOIN)
@@ -48,7 +50,7 @@ public class Join extends WorkflowSystemTask {
                             .map(name -> TaskUtils.appendIteration(name, task.getIteration()))
                             .collect(Collectors.toList());
         }
-        for (String joinOnRef : joinOn) {
+        for (String joinOnRef : Nullability.castToNonnull(joinOn)) {
             TaskModel forkedTask = workflow.getTaskByRefName(joinOnRef);
             if (forkedTask == null) {
                 // Task is not even scheduled yet
