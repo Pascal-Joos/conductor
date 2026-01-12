d8fcb5e4a1af8acc435e419821bf3e164d92a18a
Pascal-Joos <pascal.joos@outlook.de>
2026-01-12 07:59:26 +0000

    fix: Type='METHOD_NO_INIT', message='initializer method does not guarantee @NonNull fields taskDefs (line 51), workflowDefs (line 52) are initialized along all control-flow paths (remember to check for exceptions or early returns).' /home/vscode/nullness-benchmarks/conductor/core/src/main/java/com/netflix/conductor/metrics/WorkflowMonitor.java:55     public WorkflowMonitor(

--- core/src/main/java/com/netflix/conductor/metrics/WorkflowMonitor.java
+++ core/src/main/java/com/netflix/conductor/metrics/WorkflowMonitor.java
@@ -16,6 +16,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import javax.annotation.Nullable;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -30,6 +32,8 @@ import com.netflix.conductor.core.dal.ExecutionDAOFacade;
 import com.netflix.conductor.core.execution.tasks.WorkflowSystemTask;
 import com.netflix.conductor.dao.QueueDAO;
 import com.netflix.conductor.service.MetadataService;
+
+import edu.ucr.cs.riple.annotator.util.Nullability;
 
 import static com.netflix.conductor.core.execution.tasks.SystemTaskRegistry.ASYNC_SYSTEM_TASKS_QUALIFIER;
 
@@ -48,8 +52,8 @@ public class WorkflowMonitor {
     private final int metadataRefreshInterval;
     private final Set<WorkflowSystemTask> asyncSystemTasks;
 
-    private List<TaskDef> taskDefs;
-    private List<WorkflowDef> workflowDefs;
+    @Nullable private List<TaskDef> taskDefs;
+    @Nullable private List<WorkflowDef> workflowDefs;
     private int refreshCounter = 0;
 
     public WorkflowMonitor(
@@ -78,14 +82,19 @@ public class WorkflowMonitor {
                 refreshCounter = metadataRefreshInterval;
             }
 
-            workflowDefs.forEach(
-                    workflowDef -> {
-                        String name = workflowDef.getName();
-                        String version = String.valueOf(workflowDef.getVersion());
-                        String ownerApp = workflowDef.getOwnerApp();
-                        long count = executionDAOFacade.getPendingWorkflowCount(name);
-                        Monitors.recordRunningWorkflows(count, name, version, ownerApp);
-                    });
+            if (workflowDefs == null || taskDefs == null) {
+                return;
+            }
+
+            Nullability.castToNonnull(workflowDefs)
+                    .forEach(
+                            workflowDef -> {
+                                String name = workflowDef.getName();
+                                String version = String.valueOf(workflowDef.getVersion());
+                                String ownerApp = workflowDef.getOwnerApp();
+                                long count = executionDAOFacade.getPendingWorkflowCount(name);
+                                Monitors.recordRunningWorkflows(count, name, version, ownerApp);
+                            });
 
             taskDefs.forEach(
                     taskDef -> {
