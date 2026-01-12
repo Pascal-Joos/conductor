5d566ee400180157d66d7c406b76bd67db50c155
Pascal-Joos <pascal.joos@outlook.de>
2026-01-12 07:52:56 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression joinOn is @Nullable' /home/vscode/nullness-benchmarks/conductor/core/src/main/java/com/netflix/conductor/core/execution/tasks/ExclusiveJoin.java:54                     joinOn.stream()

--- core/src/main/java/com/netflix/conductor/core/execution/tasks/ExclusiveJoin.java
+++ core/src/main/java/com/netflix/conductor/core/execution/tasks/ExclusiveJoin.java
@@ -53,12 +53,12 @@ public class ExclusiveJoin extends WorkflowSystemTask {
             // If exclusive join is part of loop over task, wait for specific iteration to get
             // complete
             joinOn =
-                    joinOn.stream()
+                    Nullability.castToNonnull(joinOn).stream()
                             .map(name -> TaskUtils.appendIteration(name, task.getIteration()))
                             .collect(Collectors.toList());
         }
         TaskModel exclusiveTask = null;
-        for (String joinOnRef : joinOn) {
+        for (String joinOnRef : Nullability.castToNonnull(joinOn)) {
             LOGGER.debug("Exclusive Join On Task {} ", joinOnRef);
             exclusiveTask = workflow.getTaskByRefName(joinOnRef);
             if (exclusiveTask == null || exclusiveTask.getStatus() == TaskModel.Status.SKIPPED) {
